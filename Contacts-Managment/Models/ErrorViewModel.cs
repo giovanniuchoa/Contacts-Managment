f3a69da610ed2ff6f4cde343d7fa80ab9a1a2b04
Giovanni Lucca Reque Uchoa <gi.lucca10@gmail.com>
2024-01-11 17:13:30 -0300

    project creation

---
+++ Contacts-Managment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Contacts_Managment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
