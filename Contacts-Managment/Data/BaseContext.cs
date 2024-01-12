6ef6468d9222bef5b2ded374e2ab345f66c6f6c3
Giovanni Lucca Reque Uchoa <gi.lucca10@gmail.com>
2024-01-12 16:48:52 -0300

    Building front and installing libs

---
+++ Contacts-Managment/Data/BaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Contacts_Managment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts_Managment.Data
+{
+    public class BaseContext : DbContext
+    {
+        protected BaseContext(DbContextOptions<BaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContactModel> Contacts { get; set; }
+
+    }
+}
---
+++ Contacts-Managment/Models/ContactModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Contacts_Managment.Models
+{
+    public class ContactModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+
+    }
+}
